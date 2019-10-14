d1a7e8cd876070a5cede03d92c42f9e4fea3dfb6
aapetsi <apetsiampiah@gmail.com>
2019-10-14 13:10:15 +0000

    Initial commit

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
-// Test away!
+// Test away
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import Controls from './Controls'
+
+// afterEach(rtl.cleanup())
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = rtl.render(<Controls />)
+})
+
+describe('Dashboard component', () => {
+  test('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot()
+  })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from 'react'
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import Display from './Display'
+
+// afterEach(rtl.cleanup())
+
+let wrapper
+
+beforeEach(() => {
+  wrapper = rtl.render(<Display />)
+})
+
+describe('Dashboard component', () => {
+  test('matches the snapshot!', () => {
+    expect(wrapper.container).toMatchSnapshot()
+  })
+})
